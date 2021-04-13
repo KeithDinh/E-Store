b2f8997d6dc0cd9ae6c059ae599559c47262b052
KeithDinhcs <keithdinhcs@gmail.com>
2021-04-13 15:20:28 -0500

    add backend route get product

--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -8,9 +8,13 @@ const {
   createProduct,
   getProducts,
   removeProduct,
+  getProduct,
 } = require("../controllers/productController");
 
 router.route("/product").post(authCheck, adminCheck, createProduct);
 router.route("/products/:count").get(getProducts);
-router.route("/product/:slug").delete(authCheck, adminCheck, removeProduct);
+router
+  .route("/product/:slug")
+  .delete(authCheck, adminCheck, removeProduct)
+  .get(getProduct);
 module.exports = router;
