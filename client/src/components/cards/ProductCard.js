c4fe79518806682ccbdcadc8bf6047b9fcbcf582
KeithDinhcs <keithdinhcs@gmail.com>
2021-05-03 16:04:49 -0500

    Add a category state for product create get rid of the bugs category didn't update when using setValues()

---
+++ client/src/components/cards/ProductCard.js
@@ -0,0 +1,5 @@
+const ProductCard = () => {
+  return <div>Product</div>;
+};
+
+export default ProductCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+import { LoadingOutlined } from "@ant-design/icons";
+
+import { getProductsByCount } from "../functions/product";
+import ProductCard from "../components/cards/ProductCard";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  const loadProducts = () => {
+    setLoading(true);
+    getProductsByCount(100)
+      .then((res) => setProducts(res.data))
+      .catch((error) => console.log(error))
+      .finally(setLoading(false));
+  };
+
+  return (
+    <>
+      <div className="jumbotron">
+        {!loading ? (
+          <h4>Product create</h4>
+        ) : (
+          <LoadingOutlined className="text-danger" />
+        )}
+      </div>
+      <div className="container">
+        {products.map((p) => (
+          <ProductCard />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Home;
