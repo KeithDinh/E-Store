6e8ab67a7fdccff926665b752d5b472c65d45096
KeithDinhcs <keithdinhcs@gmail.com>
2021-07-05 20:16:16 -0500

    show order info adin dashboard

--- client/src/components/cards/ShowPaymentInfo.js
+++ client/src/components/cards/ShowPaymentInfo.js
@@ -3,25 +3,30 @@ import React from "react";
 const ShowPaymentInfo = ({ order, showStatus = true }) => {
   return (
     <div>
-      <p>
-        <span>Order Id: {order.paymentIntent.id}</span>{" "}
+      <p className="text-left">
+        <span>Order Id: {order.paymentIntent.id}</span>
+        {" | "}
         <span>
           Amount:{" "}
           {(order.paymentIntent.amount /= 100).toLocaleString("en-US", {
             style: "currency",
             currency: "USD",
           })}
-        </span>{" "}
-        <span>Currency: {order.paymentIntent.currency.toUpperCase()}</span>{" "}
+        </span>
+        {" | "}
+        <span>Currency: {order.paymentIntent.currency.toUpperCase()}</span>
+        {" | "}
         <span>Method: {order.paymentIntent.payment_method_types[0]}</span>
+        {" | "}
         <span>Payment: {order.paymentIntent.status.toUpperCase()}</span>
+        {" | "}
         <span>
           Order on:{" "}
           {new Date(order.paymentIntent.created * 1000).toLocaleTimeString()}
-        </span>{" "}
+        </span>
         {showStatus && (
           <span className="badge bg-primary text-white">
-            STATUS: {order.orderStatus}
+            {" | "} STATUS: {order.orderStatus}
           </span>
         )}
       </p>
